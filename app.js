0995e8a8e6494dad85ee8210acd616112a0dbf9c
Ahmad <ahmaddes1409@gmail.com>
2026-07-24 20:28:39 +0300

    chore(passenger): touch app.js to trigger process reload on Hostinger

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-// Hostinger Phusion Passenger / LiteSpeed Node entry point
+// Hostinger Phusion Passenger / LiteSpeed Node entry point (v1.0.4 - 2026-07-24)
 const app = require('./src/server.js');
 module.exports = app;

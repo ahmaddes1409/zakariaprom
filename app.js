a3375a8e918a58d9a4a76860d89121c35990bd44
Ahmad <ahmaddes1409@gmail.com>
2026-07-24 20:35:34 +0300

    chore(passenger): touch app.js v1.0.7 to reload LiteSpeed Node

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-// Hostinger Phusion Passenger / LiteSpeed Node entry point (v1.0.6 - 2026-07-24 20:35)
+// Hostinger Phusion Passenger / LiteSpeed Node entry point (v1.0.7 - 2026-07-24 20:36)
 const app = require('./src/server.js');
 module.exports = app;

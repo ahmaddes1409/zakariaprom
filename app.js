b168048c850db87484501e0ededcb8ea26b16bd9
Ahmad <ahmaddes1409@gmail.com>
2026-07-27 10:30:08 +0300

    chore(release): bump app.js version to v1.2.2 to trigger process reload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Hostinger LiteSpeed / Passenger Node entry point (v1.2.1)
+// Hostinger LiteSpeed / Passenger Node entry point (v1.2.2)
 const app = require('./src/server.js');
 
 module.exports = app;

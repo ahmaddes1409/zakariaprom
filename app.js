8604d61c3286e2067dc3eeb2d9320ca3db109920
Ahmad <ahmaddes1409@gmail.com>
2026-07-29 12:02:42 +0300

    chore(release): bump app.js version to v1.7.1 to reload Passenger process

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Hostinger LiteSpeed / Passenger Node entry point (v1.7.0 - Merge Karmedya & Etkin Products)
+// Hostinger LiteSpeed / Passenger Node entry point (v1.7.1 - Balanced Product Interleaving)
 const app = require('./src/server.js');
 
 module.exports = app;

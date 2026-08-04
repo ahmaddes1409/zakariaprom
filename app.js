52b57c513561388e07769b18600c4ed730b51698
Ahmad <ahmaddes1409@gmail.com>
2026-08-04 20:03:25 +0300

    chore(release): bump app.js version to v1.7.3 to reload Passenger process

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Hostinger LiteSpeed / Passenger Node entry point (v1.7.2 - Mojikake Character Encoding Repair)
+// Hostinger LiteSpeed / Passenger Node entry point (v1.7.3 - Enhanced Mojikake Encoding Repair)
 const app = require('./src/server.js');
 
 module.exports = app;

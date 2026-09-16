591c9db169ea634e7e3db7a319e0c75db72f33d2
Ahmad <ahmaddes1409@gmail.com>
2026-09-16 11:54:41 +0300

    chore(release): bump app.js version to v1.8.1 to reload Passenger process

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Hostinger LiteSpeed / Passenger Node entry point (v2.2.0 - Add category dropdown filter and quick category chips to admin products search)
+// Hostinger LiteSpeed / Passenger Node entry point (v1.8.1 - Kutulu Setler Category Mapping Update)
 const app = require('./src/server.js');
 
 module.exports = app;
